b9f65a1b8f1fec5b01330caa562ff366c3744167
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-07-18 07:05:08 +0700

    controller

---
+++ src/AuctionService/DTOs/UpdateAuctionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionService.DTOs;
+
+public class UpdateAuctionDTO
+{
+    [Required]
+    public string Make {get; set;}
+    [Required]
+    public string Model {get; set;}
+    [Required]
+    public int? Year {get; set;}
+    [Required]
+    public string Color {get; set;}
+    [Required]
+    public int? Mileage {get; set;}
+}
