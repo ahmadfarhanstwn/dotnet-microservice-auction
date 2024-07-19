7d9edad281b0e35b0d3e1519ccd6dfdbf32f44e8
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-07-19 07:11:50 +0700

    seeding data mongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,5 +11,12 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try {
+    await DBInitializer.InitDB(app);
+} catch(Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
