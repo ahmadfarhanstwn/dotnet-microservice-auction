964fbbbf5e54c301f8a8d1f618abab3b279997ed
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-08-06 06:22:49 +0700

    remaining consumer and identity service config for next app

---
+++ src/Contracts/AuctionFinished.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts;
+
+public class AuctionFinished
+{
+    public bool itemSold {get; set;}
+    public string AuctionId {get; set;}
+    public string Seller {get; set;}
+    public string Winner {get; set;}
+    public int? Amount {get; set;}
+}
