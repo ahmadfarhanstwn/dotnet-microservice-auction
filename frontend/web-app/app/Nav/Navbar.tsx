9ad5148078c4cf3598468561dc7a16e97dac14c1
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-08-27 05:37:27 +0700

    navbar

---
+++ frontend/web-app/app/Nav/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AiOutlineCar } from 'react-icons/ai'
+
+export default function Navbar() {
+  return (
+    <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
+        <div className='flex items-center gap-3 text-3xl font-semibold text-red-500'>
+            <AiOutlineCar size={34} />
+            <div>Mulyono Auctions</div>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </header>
+  )
+}
